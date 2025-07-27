1371015929c1cb249b14fbafb77ae60de0b6cecd
sdhar <sdharihardy8@yahoo.com>
2025-07-27 13:12:44 -0400

    Finished Unit 8: W8S1 Advanced Problem Set

--- Unit 8/Week8Session1.py
+++ Unit 8/Week8Session1.py
@@ -132,10 +132,128 @@ def find_flower(root, flower):
     
     return find_flower(root.left, flower) or find_flower(root.right, flower)
 
-
 flower_field = TreeNode("Rose", 
                         TreeNode("Lily", TreeNode("Orchid"), TreeNode("Lilac")),
                                 TreeNode("Daisy", None, TreeNode("Dahlia")))
 
-print(find_flower(flower_field, "Lilac"))
-print(find_flower(flower_field, "Hibiscus"))
+# print(find_flower(flower_field, "Lilac"))
+# print(find_flower(flower_field, "Hibiscus"))
+
+# Advanced Problem Set
+
+def leftmost_path(root):
+    if root is None:
+        return []
+    
+    if not root.left:
+        return [root.val]
+    
+    left = leftmost_path(root.left)
+    return [root.val] + left
+system_a = TreeNode("CaveA", 
+                  TreeNode("CaveB", TreeNode("CaveD"), TreeNode("CaveE")), 
+                          TreeNode("CaveC", None, TreeNode("CaveF")))
+system_b = TreeNode("CaveA", None, TreeNode("CaveB", None, TreeNode("CaveC")))
+
+# print(leftmost_path(system_a))
+# print(leftmost_path(system_b))
+
+def leftmost_path2(root): # Same problem as above, just done iteratively
+    result = []
+    current = root
+    result.append(current.val)
+    
+    while current.left:
+        result.append(current.left.val)
+        current = current.left
+    return result
+
+system_a = TreeNode("CaveA", 
+                  TreeNode("CaveB", TreeNode("CaveD"), TreeNode("CaveE")), 
+                          TreeNode("CaveC", None, TreeNode("CaveF")))
+system_b = TreeNode("CaveA", None, TreeNode("CaveB", None, TreeNode("CaveC")))
+# print(leftmost_path2(system_a))
+# print(leftmost_path2(system_b))
+
+def count_species(node):
+    if node is None:
+        return 0
+    
+    return 1 + count_species(node.left) + count_species(node.right)
+food_chain = TreeNode("Shark", 
+                    TreeNode("Grouper", TreeNode("Conch"), TreeNode("Tang")),
+                            TreeNode("Snapper", None, TreeNode("Zooplankton")))
+
+# print(count_species(food_chain))
+
+def explore_reef(root):
+    if root is None:
+        return []
+    
+    return [root.val] + explore_reef(root.left) + explore_reef(root.right)
+reef = TreeNode("CoralA", 
+                TreeNode("CoralB", TreeNode("CoralD"), TreeNode("CoralE")), 
+                          TreeNode("CoralC"))
+
+# print(explore_reef(reef))
+
+def get_decisions(node):
+    if not node.left or not node.right:
+        return node.val
+    
+    left = get_decisions(node.left)
+    right = get_decisions(node.right)
+
+    if node.val == "AND":
+        return left and right
+    elif node.val == "OR":
+        return left or right
+root = TreeNode("AND")
+root.left = TreeNode("OR")
+root.right = TreeNode("AND")
+root.left.left = TreeNode(True)
+root.left.right = TreeNode(False)
+root.right.left = TreeNode(True)
+root.right.right = TreeNode(False)
+# print(get_decisions(root))
+
+def is_uniform(root):
+    if root is None:
+        return True
+    
+    def search(node):
+        if node is None:
+            return True
+        if node.val != root.val:
+            return False
+        return search(node.left) and search(node.right)
+
+    return search(root)
+coral = TreeNode(1, TreeNode(1, TreeNode(1), TreeNode(1)), TreeNode(1))
+coral2 = TreeNode(1, TreeNode(2), TreeNode(1))
+
+# print(is_uniform(coral))
+# print(is_uniform(coral2))
+
+def find_largest_pearl(root):
+  if root is None:
+      return 0
+  
+  left = find_largest_pearl(root.left)
+  right = find_largest_pearl(root.right)
+
+  largest = root.val
+
+  if left > largest:
+      largest = left
+  if right > largest:
+      largest = right
+    
+  return largest
+
+    
+oysters = TreeNode(7, TreeNode(6, TreeNode(5), TreeNode(1)), TreeNode(0))
+oysters2 = TreeNode(1, TreeNode(0), TreeNode(1))
+
+print(find_largest_pearl(oysters))
+print(find_largest_pearl(oysters2))
